28fc42fb23407711b06d0c1a94f00f788f431ab0
ViviRVCC <G00330203@stu.raritanval.edu>
2025-11-12 12:50:32 -0500

    professor walk

--- Assets/01-Professor/ChangeDirection.cs
+++ Assets/01-Professor/ChangeDirection.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Toolbars;
 using UnityEngine;
 
 public class ChangeDirection : MonoBehaviour
 {
+    private Animator anim;
+    private float horizontalMove;
+    private float verticalMove;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        if (horizontalMove < 0)
+        {
+            anim.SetInteger("direction", 3);
+        }
+        if (horizontalMove > 0)
+        {
+            anim.SetInteger("direction", 1);
+        }
     }
 }
